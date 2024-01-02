41625948cf684b648461a912575deb06c4054b84
mazdul1000 <mazdul1000@gmail.com>
2024-01-02 17:24:53 +0600

    [DONE]LSA-6: Create service controller and service

---
+++ src/app/modules/services/service.controller.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express"
+import catchAsync from "../../../shared/catchAsync"
+import { ServiceServices } from "./service.service";
+import sendResponse from "../../../shared/sendResponse";
+import { IService } from "./service.interface";
+
+const createService = catchAsync(
+    async (req:Request, res:Response):Promise<void> => {
+        const data = req.body;
+        const result = await ServiceServices.createService(data);
+
+        sendResponse<IService>(res, {
+            success: true,
+            statusCode: 200,
+            message: "New service created successfully",
+            data: result
+        })
+    }
+)
+
+const getAllServices = catchAsync(
+    // Filter options
+    // eslint-disable-next-line no-unused-vars
+    async (req:Request, res:Response):Promise<void> => {
+        const result = await ServiceServices.getAllServices();
+        
+        sendResponse<IService[]>(res, {
+            success: true,
+            statusCode: 200,
+            message: "Services data retrieved successfully",
+            data: result
+        })
+    }
+)
+
+const getSingleService = catchAsync(
+    async (req: Request, res: Response):Promise<void> => {
+        const {id} = req.params;
+        const result = await ServiceServices.getSingleService(id);
+
+        sendResponse<IService>(res, {
+            success: true,
+            statusCode: 200,
+            message: "Service info retrieved successfully",
+            data: result
+        })
+    }
+)
+
+const updateService = catchAsync(
+    async (req:Request, res: Response) => {
+        const { id } = req.params;
+        const { data } = req.body;
+        const result = await ServiceServices.updateService(id, data);
+
+        sendResponse<IService>(res, {
+            success: true,
+            statusCode: 200,
+            message: "Service updated succesfully",
+            data: result
+        })
+    }
+)
+
+const deleteService = catchAsync(
+    async(req:Request, res: Response) => {
+        const { id } = req.params;
+        const result = await ServiceServices.deleteService(id);
+
+        sendResponse(res, {
+            success: true,
+            statusCode: 200,
+            message: "Service deleted successfully",
+            data: result
+        })
+    }
+)
+
+export const ServiceController ={
+    createService,
+    getAllServices,
+    getSingleService,
+    updateService,
+    deleteService
+}
---
+++ src/app/modules/services/service.service.ts
@@ -0,0 +1,38 @@
+import { IService } from "./service.interface"
+import Service from "./service.model"
+
+const createService = async(payload: IService):Promise<IService | null> => {
+    const result = await Service.create(payload);
+    return result
+}
+
+const getAllServices = async() => {
+
+    // filtering and searching functionalites
+
+    const result = await Service.find();
+    return result
+}
+
+const getSingleService = async(payload:string):Promise<IService |null> => {
+    const result = await Service.findById(payload);
+    return result;
+}
+
+const updateService = async (id: string, payload:Partial<IService>):Promise<IService |null> => {
+    const result = await Service.findOneAndUpdate({ _id: id}, payload);
+    return result;
+}
+
+const deleteService = async(payload:string):Promise<IService |null> => {
+    const result = await Service.findOneAndDelete({_id: payload});
+    return result;
+}
+
+export const ServiceServices = {
+    createService,
+    getAllServices,
+    getSingleService,
+    updateService,
+    deleteService
+}
