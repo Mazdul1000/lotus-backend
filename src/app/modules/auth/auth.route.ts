284ba0faf5b7831cf84d533dc22c1102fb42b739
mazdul1000 <mazdul1000@gmail.com>
2024-01-02 13:40:59 +0600

    [DONE] Created global error handling functionality

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -3,7 +3,10 @@ import { UserController } from '../user/user.controller'
 
 const router = Router()
 
-// Signup
-router.post('/signup', UserController.createUser)
+// create user
+router.post(
+    '/signup',
+    UserController.createUser
+  );
 
 export const AuthRoutes = router
